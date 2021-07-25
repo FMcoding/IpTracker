a28a943ac90373065c37db40b9a10f94dc1af654
TestCode <68029426+FMcoding@users.noreply.github.com>
2021-07-25 18:28:49 -0400

    Add files via upload

---
+++ TrackerJS.js
@@ -0,0 +1,14 @@
+var latlng = [39.417709935214816, -77.40642409937432];
+
+var mymap = L.map('mapid').setView([latlng[0], latlng[1]], 18);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+L.marker([latlng[0], latlng[1]]).addTo(mymap)
